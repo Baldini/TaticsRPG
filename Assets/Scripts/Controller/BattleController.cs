3a5a8c205d9b2eba999110ff6ac2c6e33358e196
Baldini <guilhermebaldini@hotmail.com>
2017-09-01 01:03:02 -0300

    Initial project

---
+++ Assets/Scripts/Controller/BattleController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+public class BattleController : StateMachine
+{
+
+    // Use this for initialization
+    public CameraRig cameraRig;
+    public Board board;
+    public LevelData levelData;
+    public Transform tileSelectionIndicator;
+    public Point pos;
+
+    public GameObject heroPrefab;
+    public Unit currentUnit;
+    public Tile currentTile { get { return board.GetTile(pos); } }
+    void Start()
+    {
+        ChangeState<InitBattleState>();
+    }
+}
